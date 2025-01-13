451b26c6cc8656d2f4de7223dde88e569140d857
hemant <hemant>
2025-01-13 21:04:04 +0530

    feat: add cloudinary urls to images

--- src/components/caseStudy/CaseStudy.jsx
+++ src/components/caseStudy/CaseStudy.jsx
@@ -1,21 +1,16 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { Heading } from "../Heading";
-import { twMerge } from "tailwind-merge";
-import Image from "next/image";
+import { useState } from 'react';
+import { Heading } from '../Heading';
+import { twMerge } from 'tailwind-merge';
+import Image from 'next/image';
+import { caseStudies } from '@/data/caseStudy';
 
 export function CaseStudy() {
-  const caseStudy = [
-    "Brand Enhancement",
-    "brand exposure",
-    "Innovating Marketing",
-  ];
+  const caseStudy = ['Brand Enhancement', 'brand exposure', 'Innovating Marketing'];
 
   const handleNextClick = () => {
-    currentIndex === caseStudy.length - 1
-      ? setCurrentIndex(0)
-      : setCurrentIndex(currentIndex + 1);
+    currentIndex === caseStudy.length - 1 ? setCurrentIndex(0) : setCurrentIndex(currentIndex + 1);
   };
 
   const handlePrevClick = () => {
@@ -26,32 +21,22 @@ export function CaseStudy() {
 
   return (
     <section className="bg-secondary" id="case-study">
-      <Heading title={"case study"} primary />
-      <div
-        id="slider"
-        className="py-6 flex w-full items-center justify-center gap-4 max-sm:gap-2"
-      >
-        <Image priority width={300} height={300}
-          id="prev"
-          src="/images/icons/left-arrow.png"
-          className="w-7 cursor-pointer"
-          alt="left-arrow"
-          onClick={handlePrevClick}
-        />
-        <div className="relative w-56 flex justify-start overflow-hidden">
-          <div
-            id="carousel"
-            style={{ translate: `-${224 * (currentIndex)}px` }}
-            className={twMerge("relative top-0 flex w-full transition-all duration-500")}
-          >
-              {caseStudy.map((title) => (
-                <div key={title} className="case-study-card slide w-52 px-2">
-                  {title}
-                </div>
-              ))}
+      <Heading title={'case study'} primary />
+      <div id="slider" className="flex w-full items-center justify-center gap-4 py-6 max-sm:gap-2">
+        <Image priority width={300} height={300} id="prev" src="/images/icons/left-arrow.png" className="w-7 cursor-pointer" alt="left-arrow" onClick={handlePrevClick} />
+        <div className="relative flex w-56 justify-start overflow-hidden">
+          <div id="carousel" style={{ translate: `-${224 * currentIndex}px` }} className={twMerge('relative top-0 flex w-full transition-all duration-500')}>
+            {caseStudy.map((title) => (
+              <div key={title} className="case-study-card slide w-52 px-2">
+                {title}
+              </div>
+            ))}
           </div>
         </div>
-        <Image priority width={300} height={300}
+        <Image
+          priority
+          width={300}
+          height={300}
           id="next"
           src="/images/icons/left-arrow.png"
           className="w-7 rotate-180 cursor-pointer"
@@ -59,32 +44,77 @@ export function CaseStudy() {
           onClick={handleNextClick}
         />
       </div>
-      <div id="image-wrapper" className="relative mx-auto py-4 w-full max-w-4xl overflow-hidden">
+      <div id="image-wrapper" className="relative mx-auto w-full max-w-4xl overflow-hidden py-4">
         <div id="image-slider" className="flex w-full">
-          <div id="brand-enhancement" className={twMerge("grid w-full grid-cols-3 gap-6 px-4 py-2 *:rounded-md max-sm:grid-cols-2 max-sm:gap-3", (currentIndex == 0) ? 'grid': 'hidden' )}>
-            <Image priority width={300} height={300} src="/images/case_study/brand_enhancement_(1).jpg" alt="brand_enhancement_1" className="aspect-[4/3] object-cover object-center shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_enhancement_(3).jpeg" alt="brand_enhancement_3" className="aspect-[4/3] object-cover object-center shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_enhancement_(4).jpeg" alt="brand_enhancement_4" className="aspect-[4/3] object-cover object-center shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_enhancement_(5).jpeg" alt="brand_enhancement_5" className="aspect-[4/3] object-cover object-center shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_enhancement_(6).jpeg" alt="brand_enhancement_6" className="aspect-[4/3] object-cover object-center shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_enhancement_(7).jpeg" alt="brand_enhancement_7" className="aspect-[4/3] object-cover object-center shadow-md shadow-black/40" />
+          <div
+            id="brand-enhancement"
+            className={twMerge('grid w-full grid-cols-3 gap-6 px-4 py-2 *:rounded-md max-sm:grid-cols-2 max-sm:gap-3', currentIndex == 0 ? 'grid' : 'hidden')}
+          >
+            {caseStudies.brandEnhancement.map(({ src, alt }) => (
+              <Image priority width={300} height={300} src={src} alt={alt} className="aspect-[4/3] object-cover object-center shadow-md shadow-black/40" />
+            ))}
           </div>
 
-          <div id="brand-exposure" className={twMerge("grid w-full grid-cols-3 gap-6 px-4 py-2 *:rounded-md max-sm:grid-cols-2 max-sm:gap-3", (currentIndex == 1) ? 'grid': 'hidden' )}>
-            <Image priority width={300} height={300} src="/images/case_study/brand_exposure_(1).jpg" alt="brand_exposure_1" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_exposure_(3).jpg" alt="brand_exposure_3" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_exposure_(4).jpg" alt="brand_exposure_4" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_exposure_(5).jpg" alt="brand_exposure_5" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_exposure_(6).jpg" alt="brand_exposure_6" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/brand_exposure_(7).jpg" alt="brand_exposure_7" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
+          <div
+            id="brand-exposure"
+            className={twMerge('grid w-full grid-cols-3 gap-6 px-4 py-2 *:rounded-md max-sm:grid-cols-2 max-sm:gap-3', currentIndex == 1 ? 'grid' : 'hidden')}
+          >
+            {caseStudies.brandExposure.map(({ src, alt }) => (
+              <Image priority width={300} height={300} src={src} alt={alt} className="aspect-[4/3] object-cover object-center shadow-md shadow-black/40" />
+            ))}
           </div>
-          <div id="brand-innovating_marketing" className={twMerge("grid w-full grid-cols-3 gap-6 px-4 py-2 *:rounded-md max-sm:grid-cols-2 max-sm:gap-3", (currentIndex == 2) ? 'grid': 'hidden' )}>
-            <Image priority width={300} height={300} src="/images/case_study/innovating_marketing_(1).jpg" alt="innovating_marketing_1" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/innovating_marketing_(2).jpg" alt="innovating_marketing_2" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/innovating_marketing_(3).jpg" alt="innovating_marketing_3" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/innovating_marketing_(4).jpg" alt="innovating_marketing_4" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/innovating_marketing_(5).jpg" alt="innovating_marketing_5" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
-            <Image priority width={300} height={300} src="/images/case_study/innovating_marketing_(6).jpg" alt="innovating_marketing_6" className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40" />
+          <div
+            id="brand-innovating_marketing"
+            className={twMerge('grid w-full grid-cols-3 gap-6 px-4 py-2 *:rounded-md max-sm:grid-cols-2 max-sm:gap-3', currentIndex == 2 ? 'grid' : 'hidden')}
+          >
+            <Image
+              priority
+              width={300}
+              height={300}
+              src="/images/case_study/innovating_marketing_(1).jpg"
+              alt="innovating_marketing_1"
+              className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40"
+            />
+            <Image
+              priority
+              width={300}
+              height={300}
+              src="/images/case_study/innovating_marketing_(2).jpg"
+              alt="innovating_marketing_2"
+              className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40"
+            />
+            <Image
+              priority
+              width={300}
+              height={300}
+              src="/images/case_study/innovating_marketing_(3).jpg"
+              alt="innovating_marketing_3"
+              className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40"
+            />
+            <Image
+              priority
+              width={300}
+              height={300}
+              src="/images/case_study/innovating_marketing_(4).jpg"
+              alt="innovating_marketing_4"
+              className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40"
+            />
+            <Image
+              priority
+              width={300}
+              height={300}
+              src="/images/case_study/innovating_marketing_(5).jpg"
+              alt="innovating_marketing_5"
+              className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40"
+            />
+            <Image
+              priority
+              width={300}
+              height={300}
+              src="/images/case_study/innovating_marketing_(6).jpg"
+              alt="innovating_marketing_6"
+              className="aspect-[4/3] object-cover object-bottom shadow-md shadow-black/40"
+            />
           </div>
         </div>
       </div>
---
+++ src/data/caseStudy.js
@@ -0,0 +1,39 @@
+export const caseStudies = {
+  brandEnhancement: [
+    {
+      src: 'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736778984/Images/brand_enhancement__7_fwsqjs.jpg',
+      alt: 'ignis',
+    },
+    {
+      src: 'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736778982/Images/brand_enhancement__4_pcm2hy.jpg',
+      alt: 'kurl-on',
+    },
+    {
+      src: 'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736778981/Images/brand_enhancement__2_pbc9av.jpg',
+      alt: 'kurl-on-1',
+    },
+    {
+      src: 'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736778981/Images/brand_enhancement__3_ahkjzo.jpg',
+      alt: 'kurl-on-2',
+    },
+    {
+      src: 'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736778980/Images/brand_enhancement__1_mujugg.jpg',
+      alt: 'excide',
+    },
+    {
+      src: 'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736778980/Images/brand_enhancement__1_bqk7se.jpg',
+      alt: 'pintola',
+    },
+  ],
+  brandExposure: [
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780065/Images/brand_exposure__5_l84w76.webp', alt:'smart bazar'},
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780064/Images/brand_exposure__5_sn0a1o.jpg', alt:'prism'},
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780063/Images/brand_exposure__4_ecv7my.webp', alt:'jk_tyres'},
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780062/Images/brand_exposure__4_jklh1p.jpg', alt:'supriya'},
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780061/Images/brand_exposure__3_veq9yk.webp', alt:'wonder_plus'},
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780060/Images/brand_exposure__3_qnphnu.webp', alt:'godrej'},
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780059/Images/brand_exposure__2_woqzbn.webp', alt:'nippon_paint'},
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780058/Images/brand_exposure__2_xj2cz5.jpg', alt:'sunsilk'},
+    {src:'https://res.cloudinary.com/mehrahemantcloud/image/upload/v1736780057/Images/brand_exposure__1_vmcxew.jpg', alt:'nutrella_max'},
+  ]
+};
